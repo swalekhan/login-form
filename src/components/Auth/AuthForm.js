1f5717c1fd9ae95d3f85d5649b99559d7d14d418
swalekhan <swalekhan1522@gmail.com>
2022-12-30 11:19:23 +0530

    sing-up

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,75 @@
+import { useRef, useState } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [spinar, setSpiner] = useState(false)
+  const [isLogin, setIsLogin] = useState(true);
+  const passRef = useRef()
+  const emailRef = useRef()
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHanler = (e) => {
+    e.preventDefault();
+    const updatedEmail = emailRef.current.value;
+    const updatedPass = passRef.current.value;
+   setSpiner(true)
+
+
+    if (isLogin) {
+    } else {
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCWiniCYxqAVOK5BkdZfgrK35YR2k8VUt8', {
+        method: 'POST',
+        body: JSON.stringify({ email: updatedEmail, password: updatedPass, returnSecureToken: true }),
+        headers: {
+          "Content-Type": "Application/json",
+        },
+
+      }).then((res) => {
+        setSpiner(false)
+        if (res.ok) {
+        } else {
+          return res.json().then((data) => {
+            let errMsg = "error"
+            if (data && data.error && data.error.message) {
+              errMsg = data.error.message;
+            }
+            alert(errMsg)
+          })
+        }
+      })
+    }
+  }
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHanler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input type='password' id='password' required ref={passRef} />
+        </div>
+        <div className={classes.actions}>
+          {!spinar &&<button>{ isLogin ? 'Login' : 'Create Account'}</button>}
+          {spinar && <p style={{color:"white"}}>sending request..</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
