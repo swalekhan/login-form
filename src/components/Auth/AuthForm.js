b5b284c5901a56105bf7c7c4695a9120199d88a8
swalekhan <swalekhan1522@gmail.com>
2022-12-30 12:19:42 +0530

    login-logic

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -16,32 +16,43 @@ const AuthForm = () => {
     e.preventDefault();
     const updatedEmail = emailRef.current.value;
     const updatedPass = passRef.current.value;
-   setSpiner(true)
+    setSpiner(true)
 
-
+    let url;
     if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCWiniCYxqAVOK5BkdZfgrK35YR2k8VUt8';
     } else {
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCWiniCYxqAVOK5BkdZfgrK35YR2k8VUt8', {
-        method: 'POST',
-        body: JSON.stringify({ email: updatedEmail, password: updatedPass, returnSecureToken: true }),
-        headers: {
-          "Content-Type": "Application/json",
-        },
-
-      }).then((res) => {
-        setSpiner(false)
-        if (res.ok) {
-        } else {
-          return res.json().then((data) => {
-            let errMsg = "error"
-            if (data && data.error && data.error.message) {
-              errMsg = data.error.message;
-            }
-            alert(errMsg)
-          })
-        }
-      })
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCWiniCYxqAVOK5BkdZfgrK35YR2k8VUt8'
     }
+
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ email: updatedEmail, password: updatedPass, returnSecureToken: true }),
+      headers: {
+        "Content-Type": "Application/json",
+      },
+
+    }).then((res) => {
+      setSpiner(false)
+      if (res.ok) {
+        return res.json();
+
+      } else {
+        return res.json().then((data) => {
+          let errMsg = "error"
+          if (data && data.error && data.error.message) {
+            errMsg = data.error.message;
+          }
+           throw new Error(errMsg)  })
+      }
+
+    }).then((data)=>{
+     console.log(data)
+    }).catch((err)=>{
+      alert(err.message)
+    })
+
   }
 
   return (
@@ -57,8 +68,8 @@ const AuthForm = () => {
           <input type='password' id='password' required ref={passRef} />
         </div>
         <div className={classes.actions}>
-          {!spinar &&<button>{ isLogin ? 'Login' : 'Create Account'}</button>}
-          {spinar && <p style={{color:"white"}}>sending request..</p>}
+          {!spinar && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {spinar && <p style={{ color: "white" }}>sending request..</p>}
           <button
             type='button'
             className={classes.toggle}
